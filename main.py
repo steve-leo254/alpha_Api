4565ef2a5d36f9c60b09f95995dc4b46243d631f
steve-leo254 <steveleo254@gmail.com>
2024-02-06 14:10:08 +0300

    2/6 commit

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import sentry_sdk
+import sentry_sdk, psycopg2
+from flask import Flask, render_template, flash
+from sentry_sdk import capture_exception
+
 
 sentry_sdk.init(
-    dsn="https://30b24fa91ed74de1b6e8fe93011d80ac@us.sentry.io/4506695594016768",
+    dsn="https://1f76b334106769b9d015a5d173862544@us.sentry.io/4506699319214080",
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     traces_sample_rate=1.0,
@@ -10,5 +13,29 @@ sentry_sdk.init(
     # We recommend adjusting this value in production.
     profiles_sample_rate=1.0,
 )
+# division_by_zero = 1 / 0
 
-division_by_zero = 1 / 0
+app=Flask(__name__)
+app.secret_key='secretkey'
+
+try:
+    conn = psycopg2.connect("dbname='template1' user='dbuser' host='localhost' password='dbpass'")
+except Exception as e:
+    capture_exception(e)
+    # print("I am unable to connect to the database")
+
+
+@app.route("/products")
+def prods():
+    try:
+        cur = conn.cursor
+        cur.execute("Select * from products")
+        prods = cur.fetchall()
+        flash("products fetched successfuly.")
+        return render_template("products.html",prods=prods)
+    except Exception as e:
+        capture_exception(e)
+        flash("Server error.Try again later.")
+        return render_template("products.html", prods=prods)
+    
+app.run()
