0c3c26c65655b789ecb8985e43fc6e98e94333c7
steve-leo254 <steveleo254@gmail.com>
2024-02-07 14:53:25 +0300

    sentry

--- main.py
+++ main.py
@@ -1,41 +1,46 @@
-import sentry_sdk, psycopg2
-from flask import Flask, render_template, flash
+import sentry_sdk
+import psycopg2
+from flask import Flask, render_template, flash, g
 from sentry_sdk import capture_exception
-
 
 sentry_sdk.init(
     dsn="https://1f76b334106769b9d015a5d173862544@us.sentry.io/4506699319214080",
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
     traces_sample_rate=1.0,
-    # Set profiles_sample_rate to 1.0 to profile 100%
-    # of sampled transactions.
-    # We recommend adjusting this value in production.
     profiles_sample_rate=1.0,
 )
-# division_by_zero = 1 / 0
 
-app=Flask(__name__)
-app.secret_key='secretkey'
+app = Flask(__name__)
+app.secret_key = 'secretkey'
 
-try:
-    conn = psycopg2.connect("dbname='template1' user='dbuser' host='localhost' password='dbpass'")
-except Exception as e:
-    capture_exception(e)
-    # print("I am unable to connect to the database")
+def get_db():
+    if 'db' not in g:
+        try:
+            g.db = psycopg2.connect("dbname='template1' user='dbuser' host='localhost' password='dbpass'")
+        except Exception as e:
+            capture_exception(e)
+            flash("Error connecting to the database.")
+    return g.db
 
+@app.teardown_appcontext
+def close_db(error):
+    if 'db' in g:
+        g.db.close()
 
 @app.route("/products")
 def prods():
+    prods = []
+
     try:
-        cur = conn.cursor
-        cur.execute("Select * from products")
+        conn = get_db()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM products")
         prods = cur.fetchall()
-        flash("products fetched successfuly.")
-        return render_template("products.html",prods=prods)
+        flash("Products fetched successfully.")
     except Exception as e:
         capture_exception(e)
-        flash("Server error.Try again later.")
-        return render_template("products.html", prods=prods)
-    
-app.run()
+        flash("Server error. Try again later.")
+
+    return render_template("products.html", prods=prods)
+
+if __name__ == '__main__':
+    app.run(debug=True)
