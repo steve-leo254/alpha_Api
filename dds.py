f550d608f33ecd2168aa9e36c87a47944e2513a4
steve-leo254 <steveleo254@gmail.com>
2024-02-13 12:35:06 +0300

    13th commit

--- dds.py
+++ dds.py
@@ -1,19 +1,18 @@
 from datetime import datetime
-from main  import db 
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 
 
 app=Flask(__name__)
 app.secret_key='secretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:leo.steve@localhost/alpha-product'
-db = SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:leo.steve@localhost/alpha-products'
+db = SQLAlchemy(app)
 
 
 
 
 class Product(db.Model):
-    __tablename__ = 'products'
+    __tablename__ = 'product'
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(100),nullable=False)
     price = db.Column(db.Float,nullable=False)
--- main.py
+++ main.py
@@ -6,19 +6,13 @@ from dds import Product,app,db
 
 sentry_sdk.init(
     dsn="https://1f76b334106769b9d015a5d173862544@us.sentry.io/4506699319214080",
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
     traces_sample_rate=1.0,
-    # Set profiles_sample_rate to 1.0 to profile 100%
-    # of sampled transactions.
-    # We recommend adjusting this value in production.
     profiles_sample_rate=1.0,
 )
-# division_by_zero = 1 / 0
 
-@app.before_first_request
-def create_table():
-    db.create_all()
+# @app.before_first_request
+# def create_table():
+
 
 @app.route("/products", methods=["POST","GET"])
 def prods():
@@ -29,11 +23,17 @@ def prods():
             return "success"
     
         except Exception as e:
-         capture_exception(e)
-        return "error"
+        #  capture_exception(e)
+         return "error"
     else:
         if request.is_json:
             data = request.json
-            print
-    
-app.run()
+            print(data)
+            return "stored"
+        else:
+            return "data is not json"
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
