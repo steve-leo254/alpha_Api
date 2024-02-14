e67d94808fb220fe657220f1895e396efb28f46e
steve-leo254 <steveleo254@gmail.com>
2024-02-14 12:30:41 +0300

    val commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sentry_sdk
-from flask import request
+from flask import request,jsonify
 from sentry_sdk import capture_exception
 from dds import Product,app,db
 
@@ -13,25 +13,46 @@ sentry_sdk.init(
 # @app.before_first_request
 # def create_table():
 
-
-@app.route("/products", methods=["POST","GET"])
+#Get all product and store
+@app.route("/products", methods=["POST","GET","PUT","PATCH","DELETE"])
 def prods():
     if request.method == "GET":
         try:
             prods = Product.queryall()
-            print(prods)
-            return "success"
+            res = []
+            for i in prods:
+                res = ({"id": i.id,"name": i.name,"price": i.price})
+            return jsonify(res),200
     
         except Exception as e:
         #  capture_exception(e)
-         return "error"
-    else:
+            return jsonify({"Error":"Server error storing product."}),500
+    elif request.method ==  "POST":
         if request.is_json:
-            data = request.json
-            print(data)
-            return "stored"
+            try:
+                data = request.json
+                print(type(data))
+                new_data = Product(name=data['name'], price= data['price'])
+                db.session.add(new_data)
+                db.session.commit()
+                r = f'Successfully stored product id" : {str(new_data.id)}'
+                res = {"Result" : r}
+                return jsonify(res),201
+            except Exception as e:
+                print(e)
+                return jsonify({"Error":"Server error storing product."}),500
         else:
-            return "data is not json"
+            return jsonify({"Error":"Method is not json"}),400
+        
+
+
+#Task by Thursday
+#Get a single product in the route
+#Create a new project call it alpha-app,make it boostrapand datatables enabled with dummy data(products) in a table
+#Have a form in a bootstrap modal with products input
+#Push your latest code. Create a new repo on github for alpha-app
+        
+
 
 if __name__ == "__main__":
     with app.app_context():
