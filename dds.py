f7725d2dfb8811b11e11077263e9a6785de5d8cf
steve-leo254 <steveleo254@gmail.com>
2024-02-26 11:15:36 +0300

    mon commit

--- dds.py
+++ dds.py
@@ -16,10 +16,10 @@ class Product(db.Model):
     name = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Float, nullable=False)
     # created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    sales = db.relationship("Sales", backref='product')
+    sale = db.relationship("Sale", backref='product')
 
-class Sales(db.Model):
-    __tablename__ = 'sales'
+class Sale(db.Model):
+    __tablename__ = 'sale'
     id = db.Column(db.Integer, primary_key=True)
     pid = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)  
     quantity = db.Column(db.Integer, nullable=False)
--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import sentry_sdk
 from flask import flash, jsonify, request
 from sentry_sdk import capture_exception
 from flask_sqlalchemy import SQLAlchemy
-from  dds import Product, app, db,Sales
+from  dds import Product, app, db,Sale
 from flask_cors import CORS
-
+import requests
 
 sentry_sdk.init(
     dsn="https://1f76b334106769b9d015a5d173862544@us.sentry.io/4506699319214080",
@@ -80,13 +80,13 @@ def get_product(product_id):
 
 
 
-@app.route('/sales',methods=['GET','POST'])
+@app.route('/sale',methods=['GET','POST'])
 def sales():
     if request.method == 'GET':
         try:
-            sales=Sales.query.all()
+            sales=Sale.query.all()
             s_dict=[]
-            for sale in sales:
+            for sale in sale:
                 s_dict.append({"id": sale.id, "pid": sale.pid, "quantity": sale.quantity,"created_at": sale.created_at})
             return jsonify(s_dict)
         except Exception as e:
@@ -98,7 +98,7 @@ def sales():
         if request.is_json:
             try:
                 data = request.json
-                new_sale = Sales(pid=data.get(
+                new_sale = Sale(pid=data.get(
                     'pid'), quantity=data.get('quantity'))
                 db.session.add(new_sale)
                 db.session.commit()
@@ -117,12 +117,14 @@ def sales():
 
 
 
-# Task by Thursday
-# Get a single product in the route
-# Create a new project call it alpha-app,make it boostrapand datatables enabled with dummy data(products) in a table
-# Have a form in a bootstrap modal with products input
-# Push your latest code. Create a new repo on github for alpha-app
 
+@app.route("/dashboard",methods=['GET'])
+def dashboard():
+    apikey = "G10JJR1J6E7WZRZQ."
+    url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=KES&apikey="+apikey
+    r = requests.get(url)
+    print("Response is.....", r.json())
+    return "Success"
 
 
 
@@ -132,3 +134,14 @@ if __name__ == "__main__":
     app.run(debug=True)
 
 
+
+
+
+# Get a single product in the route =done
+# Create a new project call it alpha-app,make it boostrapand datatables enabled with dummy data(products) in a table = done
+# Have a form in a bootstrap modal with products input = done 
+# Push your latest code. Create a new repo on github for alpha-app = done 
+    
+#  Thursday tas
+# display your currency in your web app
+#comsume  other public API
